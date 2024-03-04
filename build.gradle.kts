b5bf4521d34d80cdc68ae4acb3d75318f7ea77a8
davide magli <davide.magli99@gmail.com>
2024-03-04 16:40:05 +0100

    1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.9.0"
-    id("org.jetbrains.intellij") version "1.15.0"
+    id("org.jetbrains.intellij") version "1.17.1"
     id("org.jetbrains.compose") version "1.5.2"
 }
 
@@ -28,25 +28,23 @@ dependencies {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2022.2.5")
+    version.set("2023.1")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf("org.jetbrains.kotlin"))
 }
-
+kotlin {
+    jvmToolchain(17)
+}
 tasks {
+    wrapper {
+        gradleVersion = "8.2"
+    }
     // Set the JVM compatibility versions
-    withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
-    }
 
     patchPluginXml {
-        sinceBuild.set("222")
-        untilBuild.set("232.*")
+        sinceBuild = "222.*"
+        untilBuild = ""
     }
 
     signPlugin {
